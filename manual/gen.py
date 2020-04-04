48476afc40e5f1d78dcf4ec5e9ba8267e2cdce94
noword <noword.forever@gmail.com>
2020-04-04 10:12:44 +0800

    check the time of last modification, skip that not be modified

--- manual/gen.py
+++ manual/gen.py
@@ -3,6 +3,14 @@
 import os
 
 
+def need_update(xcf_name, jpg_name):
+    if os.path.exists(jpg_name):
+        xcf_time = os.path.getmtime(xcf_name)
+        jpg_time = os.path.getmtime(jpg_name)
+        return xcf_time > jpg_time
+    return True
+
+
 def gen_pdf(name):
     count = 1
     jpgs = []
@@ -12,12 +20,14 @@ def gen_pdf(name):
         xcf_name = f'{page_name}.xcf'
         if os.path.exists(xcf_name):
             jpg_name = f'{page_name}.jpg'
-            print(f'{xcf_name} ==> {jpg_name}')
-            os.system(f'convert.exe {xcf_name} -flatten {jpg_name}')
+            if need_update(xcf_name, jpg_name):
+                print(f'{xcf_name} ==> {jpg_name}')
+                os.system(f'convert.exe {xcf_name} -flatten {jpg_name}')
             jpgs.append(jpg_name)
         else:
             break
 
+    print(f'generating {name}.pdf')
     os.system(f'convert.exe {" ".join(jpgs)} {name}.pdf')
 
 
