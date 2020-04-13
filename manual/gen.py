92e44030ca870ce890d9fb104b12816e647ce75c
noword <noword.forever@gmail.com>
2020-04-13 13:40:00 +0800

    update pdf only when it's needed

--- manual/gen.py
+++ manual/gen.py
@@ -14,6 +14,7 @@ def need_update(xcf_name, jpg_name):
 def gen_pdf(name):
     count = 1
     jpgs = []
+    update = False
     while True:
         page_name = f'{name}_{count:02d}'
         count += 1
@@ -21,14 +22,16 @@ def gen_pdf(name):
         if os.path.exists(xcf_name):
             jpg_name = f'{page_name}.jpg'
             if need_update(xcf_name, jpg_name):
+                update = True
                 print(f'{xcf_name} ==> {jpg_name}')
                 os.system(f'convert.exe {xcf_name} -flatten {jpg_name}')
             jpgs.append(jpg_name)
         else:
             break
 
-    print(f'generating {name}.pdf')
-    os.system(f'convert.exe {" ".join(jpgs)} {name}.pdf')
+    if update:
+        print(f'generating {name}.pdf')
+        os.system(f'convert.exe {" ".join(jpgs)} {name}.pdf')
 
 
 if __name__ == '__main__':
